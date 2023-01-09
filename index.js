c9179ad2bc006708aba0db7380669e6b5d7d5823
vishalicious213 <vishalicious213@gmail.com>
2023-01-09 14:20:58 -0800

    solved problem 1

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ console.log("Array Cardio Day 1 💪")
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
+let fifteens = []
+fifteens = inventors.filter(inventor => inventor.year >= 1500 && inventor.year < 1600)
+console.table(fifteens)
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
